7e19012d96597b7c9f253cabf7c248eb4c9c31e4
TheJudyEssam <ju121.farouk@gmail.com>
2025-05-10 23:45:59 +0300

    dep

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
